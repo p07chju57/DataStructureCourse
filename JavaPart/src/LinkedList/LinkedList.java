8a630cf114e053b65d69a07ab2b0875c50b238d2
POTCHONA Justin <potchjust@gmail.com>
2020-01-31 10:42:32 +0000

    Init the repository

---
+++ JavaPart/src/LinkedList/LinkedList.java
@@ -0,0 +1,25 @@
+package LinkedList;
+
+public class LinkedList {
+
+    public static void main(String[]args){
+        Node head=new Node(4);
+        Node second=new Node(5);
+        Node third=new Node(6);
+        head.next=second;
+        second.next=third;
+        System.out.println("next after the head is "+head.next);
+        System.out.println(countNode(head));
+    }
+
+    public static int countNode(Node head){
+        int count=1;
+        Node current=head;
+
+        while (current.next!=null){
+            current=current.next;
+            count=count+1;
+        }
+        return count;
+    }
+}
---
+++ JavaPart/src/LinkedList/Node.java
@@ -0,0 +1,11 @@
+package LinkedList;
+
+public class Node {
+    int _data;
+    Node next;
+
+    public Node(int data){
+        this._data=data;
+    }
+
+}
